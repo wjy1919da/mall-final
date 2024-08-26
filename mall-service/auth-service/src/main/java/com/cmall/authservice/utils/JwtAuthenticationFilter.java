08a605ed91e21eef186ffdf54293d4dc23c7e007
wjy1919da <wjy1917886@gmail.com>
2024-08-26 02:56:20 -0400

    fix
    fix

--- mall-service/auth-service/src/main/java/com/cmall/authservice/utils/JwtAuthenticationFilter.java
+++ mall-service/auth-service/src/main/java/com/cmall/authservice/utils/JwtAuthenticationFilter.java
@@ -8,6 +8,10 @@ import org.springframework.security.web.authentication.WebAuthenticationDetailsS
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
@@ -20,8 +24,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request,
-            HttpServletResponse response,
-            FilterChain filterChain) throws ServletException, IOException {
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
         // get JWT (token) from http request
         String token = getJWTfromRequest(request);
         // validate token
